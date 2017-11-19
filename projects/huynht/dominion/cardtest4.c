34433fed8c4db9dea048a6802c9e3f3371ca0c53
Trung <huynht@oregonstate.edu>
2017-11-18 20:50:38 -0800

    Updates

---
+++ projects/huynht/dominion/cardtest4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "dominion.h"
+
+/* Gardens Card Test */
+
+int main() {
+	int bugs = 0;
+
+	struct gameState *state1 = newGame();
+	struct gameState *state2 = newGame();
+
+	state1->hand[0][0] = gardens;
+
+	memcpy(state2, state1, sizeof(struct gameState));
+
+	playCard(0, 0, 0, 0, state1);
+
+	printf("Gardens Test starting...\n");
+
+	if (memcmp(state1, state2, sizeof(struct gameState)) != 0) {
+		printf("The game state changed after card was played.\n");
+		bugs++;
+	}
+
+	if (bugs > 0)
+		printf("Gardens Test: %d bugs.\n", bugs);
+	else
+		printf("Gardens Test: Test was successful.\n");
+
+	return 0;
+}
---
+++ projects/huynht/dominion/unittest2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "dominion.h"
+
+/* Unit Test for buy card */
+
+int main() {
+	int i;
+	int bugs, found = 0;
+
+	struct gameState state;
+
+	state.deckCount[0] = 5;
+	state.deck[0][0] = copper;
+	state.deck[0][1] = copper;
+	state.deck[0][2] = copper;
+	state.deck[0][3] = copper;
+	state.deck[0][4] = copper;
+	state.deck[0][5] = copper;
+	state.deck[0][6] = copper;
+
+	state.deck[0][7] = estate;
+	state.deck[0][8] = estate;
+	state.deck[0][9] = estate;
+
+	buyCard(mine, &state);
+
+	printf("Starting test for Unit Test 2...\n");
+
+	if (state.deckCount[0] > 11) {
+		printf("The player has received too many cards.\n");
+		bugs++;
+	}
+
+	for (i = 0; i < state.deckCount[0]; i++) {
+		if (state.deck[0][i] == mine) {
+			found = 1;
+			break;
+		}
+	}
+
+	if (!found) {
+		printf("Card was not added to deck.\n");
+		bugs++;
+	}
+
+	if (bugs > 0)
+		printf("Unit Test 2: %d bugs.\n", bugs);
+	else
+		printf("Unit Test 2 was successful.\n");
+
+	return 0;
+}
